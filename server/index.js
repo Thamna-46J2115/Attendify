444dd3c19913f57c46f9b0f7cde49207e9e4a367
Thamna <46J2115@utas.edu.om>
2025-12-20 03:58:02 +0400

    remove frontend serving from backend for render

--- server/index.js
+++ server/index.js
@@ -217,13 +217,10 @@ app.use("/api/auth", authRoutes);
 
 // ================= FRONTEND (PRODUCTION ONLY) =================
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "client", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "..", "client", "build", "index.html")
-    );
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+  
 }
 
 // ================= START =================
